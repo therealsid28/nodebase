1e68a49a7046e1c4e7a16c6f39f8964b4962e162
Siddharth <moonmars2au@gmail.com>
2025-11-14 15:19:47 +0000

    inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-
 const Page = async () => {
-  await requireAuth();
-
   return <div className="">Protected Server component</div>;
 };
 
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,26 @@
-import { z } from "zod";
+import { email, z } from "zod";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.user.findMany({
       where: {
         id: ctx.auth.user.id,
+      },
+    });
+  }),
+
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "siddhareth@gmail.com",
+      },
+    });
+    return prisma.workflow.create({
+      data: {
+        name: "Test Workflow",
       },
     });
   }),
